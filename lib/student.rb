04960f264ffefb86ff808086de886723dfc4d578
Michael Evans <michael@michaelgregoryevans.com>
2017-11-13 22:46:22 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,8 +56,6 @@ class Student
   #end
 
   def self.create(name, grade)
-    student = Student.new(name, grade)
-    student.save
-    binding.pry
+    student = Student.new(name, grade).tap{|student| student.save}
   end
 end
