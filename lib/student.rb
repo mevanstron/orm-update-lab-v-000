7c57269c9e593dc181cece0dba493d377da611c5
Michael Evans <michael@michaelgregoryevans.com>
2017-11-13 22:57:26 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,4 +62,13 @@ class Student
   def self.new_from_db(row)
     Student.new(row[0], row[1], row[2]).tap{|student| student.save}
   end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM students WHERE name = ?
+      SQL
+
+    DB[:conn].execute(sql, name).tap{|student| self.new_from_db(student)}
+  end
+  
 end
